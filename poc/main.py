a98caf3cb7029634c6425266e54ada6a4503a5fc
Jinuj JN <jinuj.j.n@gmail.com>
2026-09-09 11:09:08 +0530

    port corrected

--- poc/main.py
+++ poc/main.py
@@ -26,4 +26,4 @@ async def serve_default_page():
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("main:app", host="127.0.0.1", port=4021, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=4021, reload=True)
